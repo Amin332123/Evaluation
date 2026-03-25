f4aacad4c48b7af66176239a964d73007fef7de5
Amin332123 <mohamedaminerahmani6@gmail.com>
2026-03-25 09:13:00 +0000

    finished flatten function

--- Js/TuesDay/Part2/Ex13.js
+++ Js/TuesDay/Part2/Ex13.js
@@ -1,7 +1,10 @@
-// function flatten(tableau)  {
-//     var nestedTable = tableau;
-//     nestedTable.array.forEach(e => {
-//         if (is)
-        
-//     });
-// }
+
+
+function flatten(table) {
+    if(!Array.isArray(table)) return [table];
+    return table.reduce((acc, v) => {
+        return acc.concat(flatten(v));
+    }, []);
+}
+
+console.log(flatten([1, [2, 3], [4, [5, 6]]]));
